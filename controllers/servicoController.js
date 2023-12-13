ee11ea2a1eaea4893b274a1919f3874ca9f2671b
Thiago Terra Fernandes <thiagoterra@Thiagos-MacBook-Air.local>
2023-12-13 19:15:27 -0300

    Implementação de rota para cadastrar novo servico no banco de dados

--- controllers/servicoController.js
+++ controllers/servicoController.js
@@ -1,7 +1,21 @@
 const { Servico: ServicoModel } = require("../models/Servico");
 
 const servicoController = {
+    create: async (req, res) => {
+        try {
+            const servico = {
+                nome: req.body.nome,
+                descricao: req.body.descricao,
+                preco: req.body.preco,
+            };
 
-}
+            const response = await ServicoModel.create(servico);
+
+            res.status(201).json({ response, msg: "Serviço criado com sucesso!" });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+};
 
 module.exports = servicoController;
--- routes/servicos.js
+++ routes/servicos.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 const servicoController = require("../controllers/servicoController");
 
-// Funções
+router
+    .route("/servicos")
+    .post((req, res) => servicoController.create(req, res));
 
 module.exports = router;
